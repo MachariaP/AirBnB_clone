ed37eeb490d17971ef26d50519440191bf2a528b
dinarwanjiru <dinarwanjiru77@gmail.com>
2024-02-10 10:01:48 +0000

    airbnb_clone

---
+++ models/base_model.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""BaseModel class"""
+from uuid import uuid4
+from datetime import datetime
+
+
+class BaseModel:
+    """
+    class that defines the base attributes for other classes"""
+
+    def __init__(self, *args, **kwargs):
+        """
+        constructor of a class
+        initializes BaseModel with provided arguments"""
+
+        from models import storage
+        if kwargs is not None and kwargs != {}:
+            for key in kwargs:
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(
+                        kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(
+                        kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
+
+    def __str__(self):
+        """Define string representation of BaseModel object
+        """
+
+        return "[{}] ({}) {}".format(type(self).__name__, self.id, self.__dict__)
+
+    def save(self):
+        """updates the public instance attribute updated_at"""
+
+        from models import storage
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        """This returns a dictionary containing all keys/values of __dict__"""
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = type(self).__name__
+        my_dict["created_at"] = my_dict["created_at"].isoformat()
+        my_dict["updated_at"] = my_dict["updated_at"].isoformat()
+        return my_dict
---
+++ models/engine/file_storage.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+""""Module for FileStorage class."""
+import os
+import datetime
+import json
+
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+
+class FileStorage:
+    """This is the class for storing and retrieving data"""
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """This returns the dictionary __objects"""
+        return self.__objects
+
+    def new(self, obj):
+        """This sets in __objects the obj with key <obj class name>.id"""
+
+        self.__objects["{}.{}".format(obj.__class__.__name__, obj.id)] = obj
+
+    def save(self):
+        """function that serializes __objects to the JSON file"""
+
+        with open(self.__file_path, "w") as f:
+            j_dict = {}
+            for y, z in self.__objects.items():
+                j_dict[y] = z.to_dict()
+            json.dumps(j_dict, f)
+
+    def reload(self):
+        """returns the valid attributes and their types for classname"""
+
+        try:
+            with open(self.__file_path, encoding="utf-8") as f:
+                for obj in json.load(f).values():
+                    self.new(eval(obj["__class__"])(**obj))
+        except FileNotFoundError:
+            return
