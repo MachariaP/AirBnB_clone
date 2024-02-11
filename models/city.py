fbb762a2bb955fd28dd4aab6d1536e70fc94a63f
MachariaP <walburphinehas78@gmail.com>
2024-02-11 22:36:17 +0300

    validicated pycodestyle

--- models/city.py
+++ models/city.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
-"""module for the City model"""
+
+"""Module for the City model."""
+
 from models.base_model import BaseModel
 
 
 class City(BaseModel):
-    """This is the class for managing city objects"""
+    """Class for managing city objects."""
+
     state_id = ""
-    name  = ""    
+    name = ""
